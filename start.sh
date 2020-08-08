1093ddfceeb0f93108e43fad6207b87a0bb815f7
zkan <zkan.cs@gmail.com>
2020-08-08 13:02:08 +0700

    Add Hadoop and Hive docker by Marc Lamberti :bear:

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build the base images from which are based the Dockerfiles
+# then Startup all the containers at once
+docker build -t hadoop-base docker/hadoop/hadoop-base && \
+docker build -t hive-base docker/hive/hive-base && \
+docker-compose up -d --build
